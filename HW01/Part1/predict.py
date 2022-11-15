b59df110a95b1cba8da3fd6c8c4a28e165584025
reza adinepour <r3zaadinep0ur@gmail.com>
2022-11-15 15:18:57 +0330

    initial commit

--- HW01/Part1/predict.py
+++ HW01/Part1/predict.py
@@ -1,3 +1,12 @@
+#           ******************************************************
+#          **   course         : Neural Networks                 **
+#         ***   HomeWork       : 01                              ***
+#        ****   Topic          : Single Layer Perceptron         ****
+#        ****   AUTHOR         : Reza Adinepour                  ****
+#         ***   Student ID:    : 9814303                         ***
+#          **   Github         : github.com/reza_adinepour/      **
+#           ******************************************************
+
 import numpy as np
 from unitStep import unitStep
 
